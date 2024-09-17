2bb23f705cca2a4070723c4c28f107bdb6e29962
paracorrea <paracorrea@gmail.com>
2024-09-17 19:17:36 -0300

    alterações em peso para testes

--- src/main/java/com/ceasacampinas/telnetClient/controller/BalancaController.java
+++ src/main/java/com/ceasacampinas/telnetClient/controller/BalancaController.java
@@ -4,14 +4,9 @@ import com.ceasacampinas.telnetClient.domain.Balanca;
 import com.ceasacampinas.telnetClient.service.TelnetClient;
 
 import java.beans.PropertyEditorSupport;
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -19,8 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class BalancaController {
@@ -52,9 +46,17 @@ public class BalancaController {
     	 //BigDecimal pesoCapturado = capturarPeso();  // Chama o método de teste para capturar o peso
     	BigDecimal pesoCapturado = telnetClient.capturarPeso(); // metodo correto
     	
+    	  //BigDecimal pesokg = pesoCapturado.divide(BigDecimal.valueOf(1000), 0, RoundingMode.DOWN);
+         
+    	  // Verifica se o peso resultante é menor que 5 kg
+         // if (pesokg.compareTo(BigDecimal.valueOf(5)) < 0) {
+          	// Elimina o valor ou trata como necessário, por exemplo, definindo o peso como zero ou outra ação
+         // 	pesokg = BigDecimal.ZERO; // ou outro tratamento que desejar
+         // }
     	
-    	 // BigDecimal peso = new BigDecimal(pesoCapturado).divide(BigDecimal.valueOf(1000));
+          // BigDecimal peso = new BigDecimal(pesoCapturado).divide(BigDecimal.valueOf(1000));
         // Instância de Balanca com valores padrão (caso queira preencher alguns campos previamente)
+       
         Balanca balanca = new Balanca();
         balanca.setDataPesagem(LocalDateTime.now()); // Define a data atual para a pesagem
         balanca.setPeso(pesoCapturado);  // Define o peso capturado
@@ -62,40 +64,44 @@ public class BalancaController {
 
         System.out.println("Peso capturado no controller: "+pesoCapturado);
         
-        BigDecimal pesokg = pesoCapturado.divide(BigDecimal.valueOf(1000));
+       // BigDecimal pesokg = pesoCapturado.divide(BigDecimal.valueOf(1000));
+
+       
         
         // Adiciona a instância ao modelo para que o formulário a utilize
         model.addAttribute("balanca", balanca);
-        model.addAttribute("pesoCapturado", pesokg);  // Passa o peso capturado para o modelo
+        model.addAttribute("pesoCapturado", pesoCapturado);  // Passa o peso capturado para o modelo
 
         return "balanca"; // Nome da página HTML do formulário
     }
-    @PostMapping("/capturar")
-    public String capturarPeso(@RequestParam String proprietarioCaminhao,
-                               @RequestParam String motoristaCaminhao,
-                               @RequestParam String modeloCaminhao,
-                               @RequestParam String nomeBalanceiro,
-                               @RequestParam String placaVeiculo,
-                               Model model) {
-        // Chama o service para capturar o peso da balança
-        BigDecimal pesoCapturado = telnetClient.capturarPeso();
-        
-        System.out.println("Peso capturado para o front: " +pesoCapturado);
-        
-        if (pesoCapturado == null) {
-            System.out.println("Erro peso não capturado");
-        }
-
-        // Adiciona os dados ao modelo com nomes corretos para evitar sobrescrever
-        model.addAttribute("proprietarioCaminhao", proprietarioCaminhao);
-        model.addAttribute("motoristaCaminhao", motoristaCaminhao);
-        model.addAttribute("modeloCaminhao", modeloCaminhao);
-        model.addAttribute("nomeBalanceiro", nomeBalanceiro);
-        model.addAttribute("placaVeiculo", placaVeiculo);
-        model.addAttribute("pesoCapturado", pesoCapturado);
-
-        return "redirect:/balanca";  // Retorna à página com os dados
-    }
+	/*
+	 * @PostMapping("/capturar") public String capturarPeso(@RequestParam String
+	 * proprietarioCaminhao,
+	 * 
+	 * @RequestParam String motoristaCaminhao,
+	 * 
+	 * @RequestParam String modeloCaminhao,
+	 * 
+	 * @RequestParam String nomeBalanceiro,
+	 * 
+	 * @RequestParam String placaVeiculo, Model model) { // Chama o service para
+	 * capturar o peso da balança BigDecimal pesoCapturado =
+	 * telnetClient.capturarPeso();
+	 * 
+	 * System.out.println("Peso capturado para o front: " +pesoCapturado);
+	 * 
+	 * if (pesoCapturado == null) { System.out.println("Erro peso não capturado"); }
+	 * 
+	 * // Adiciona os dados ao modelo com nomes corretos para evitar sobrescrever
+	 * model.addAttribute("proprietarioCaminhao", proprietarioCaminhao);
+	 * model.addAttribute("motoristaCaminhao", motoristaCaminhao);
+	 * model.addAttribute("modeloCaminhao", modeloCaminhao);
+	 * model.addAttribute("nomeBalanceiro", nomeBalanceiro);
+	 * model.addAttribute("placaVeiculo", placaVeiculo);
+	 * model.addAttribute("pesoCapturado", pesoCapturado);
+	 * 
+	 * return "redirect:/balanca"; // Retorna à página com os dados }
+	 */
 
    
 
@@ -126,33 +132,28 @@ public class BalancaController {
         return "redirect:/balanca";
     }
     
-    @PostMapping("/balanca/imprimirZPL")
-    public String imprimirZPL(@ModelAttribute Balanca balanca, Model model) throws IOException {
-        // Simulação da captura de peso
-        
-    	balanca.setPeso(telnetClient.capturarPeso());
-    	//balanca.setPeso(telnetClient.capturarPeso());
-        balanca.setDataPesagem(LocalDateTime.now());
-
-        // Gera o código ZPL
-        String zpl = telnetClient.gerarEtiquetaZPL(
-            balanca.getPlaca(),
-            balanca.getDestino(),
-            balanca.getValor().toString(),
-            balanca.getDataPesagem(),
-            balanca.getPeso()
-        );
-
-        // Retorna o código ZPL gerado para ser exibido na página
-        model.addAttribute("zplCode", zpl);
-        model.addAttribute("mensagem", "ZPL gerado com sucesso!");
-        
-        telnetClient.salvarEmArquivo("zpl.txt", zpl);
-        return "redirect:/balanca";  // Página que exibirá o ZPL gerado
-    }
-
-    // Método fictício para capturar o peso
-   
+	/*
+	 * @PostMapping("/balanca/imprimirZPL") public String
+	 * imprimirZPL(@ModelAttribute Balanca balanca, Model model) throws IOException
+	 * { // Simulação da captura de peso
+	 * 
+	 * balanca.setPeso(telnetClient.capturarPeso());
+	 * //balanca.setPeso(telnetClient.capturarPeso());
+	 * balanca.setDataPesagem(LocalDateTime.now());
+	 * 
+	 * // Gera o código ZPL String zpl = telnetClient.gerarEtiquetaZPL(
+	 * balanca.getPlaca(), balanca.getDestino(), balanca.getValor().toString(),
+	 * balanca.getDataPesagem(), balanca.getPeso() );
+	 * 
+	 * // Retorna o código ZPL gerado para ser exibido na página
+	 * model.addAttribute("zplCode", zpl); model.addAttribute("mensagem",
+	 * "ZPL gerado com sucesso!");
+	 * 
+	 * telnetClient.salvarEmArquivo("zpl.txt", zpl); return "redirect:/balanca"; //
+	 * Página que exibirá o ZPL gerado }
+	 * 
+	 * // Método fictício para capturar o peso
+	 */   
     private BigDecimal converterParaBigDecimal(String valor) {
         // Substitui vírgula por ponto para fazer a conversão correta
         String valorFormatado = valor.replace(".", "").replace(",", ".");
--- src/main/java/com/ceasacampinas/telnetClient/service/TelnetClient.java
+++ src/main/java/com/ceasacampinas/telnetClient/service/TelnetClient.java
@@ -3,6 +3,7 @@ package com.ceasacampinas.telnetClient.service;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -82,16 +83,25 @@ public class TelnetClient {
 
             // Remover caracteres não numéricos (exceto ponto ou vírgula, se necessário)
             String pesoFiltrado = pesoCapturado.replaceAll("[^\\d]", ""); // Mantém apenas os dígitos
+           
+            
             
             if (!pesoFiltrado.isEmpty()) {
                 // Convertendo a string para BigDecimal, assumindo que o peso está em gramas e dividindo por 1000 para obter em kg
-                peso = new BigDecimal(pesoFiltrado).divide(BigDecimal.valueOf(1000));
+                peso = new BigDecimal(pesoFiltrado);
+                peso = peso.divide(BigDecimal.valueOf(1000), 0, RoundingMode.DOWN);
             }
+            
+            if (peso.compareTo(BigDecimal.valueOf(2)) < 0) {
+              	// Elimina o valor ou trata como necessário, por exemplo, definindo o peso como zero ou outra ação
+              	peso = BigDecimal.ZERO; // ou outro tratamento que desejar
+              }
             
             System.out.println("Peso recebido: " + peso);
             socket.close();
         } catch (Exception e) {
             System.err.println("Erro ao conectar ao equipamento: " + e.getMessage());
+            //peso = BigDecimal.ZERO;
         }
 
         return peso;  // Retorna o peso capturado para ser utilizado na aplicação
@@ -103,11 +113,10 @@ public class TelnetClient {
     public void imprimirEtiqueta(String placa, String destino, String valor, LocalDateTime dataPesagem, BigDecimal peso) {
         // Obter o peso capturado
        
-    	 int contador = lerContador();
+    	    	
+    	 //BigDecimal pesokg = peso.divide(BigDecimal.valueOf(1000));  // Dividir por 1000 para converter gramas para kg
     	
-    	 BigDecimal pesokg = peso.divide(BigDecimal.valueOf(1000));  // Dividir por 1000 para converter gramas para kg
-    	
-        if (pesokg == null) {
+        if (peso == null) {
         	System.out.println("Erro na captura do peso");  // Tratar o caso de falha na captura do peso
         	
         }
@@ -125,12 +134,11 @@ public class TelnetClient {
                 "^FT115,215^A0N,23,25^FH\\^CI28^FD" + placa + "^FS^CI27\n" +  // Placa do veículo
                 "^FT30,182^A0N,23,25^FH\\^CI28^FDDestino:^FS^CI27\n" +
                 "^FT115,182^A0N,23,25^FH\\^CI28^FD" + destino + "^FS^CI27\n" +  // Destino
-                "^FT30,255^A0N,23,25^FH\\^CI28^FDValor:^FS^CI27\n" +
-                "^FT115,254^A0N,23,25^FH\\^CI28^FD" + valor + "^FS^CI27\n" +  // Valor
-                "^FT125,114^A0N,39,51^FH\\^CI28^FD" + pesokg + "kg^FS^CI27\n" +  // Peso
+                "^FT30,255^A0N,26,28^FH\\^CI28^FDValor R$:^FS^CI27\n" +
+                "^FT115,254^A0N,26,28^FH\\^CI28^FD" + valor + "^FS^CI27\n" +  // Valor
+                "^FT125,114^A0N,39,51^FH\\^CI28^FD" + peso+ "kg^FS^CI27\n" +  // Peso
                 "^FT30,33^A0N,23,23^FH\\^CI28^FD" + dataFormatada + "^FS^CI27\n" +  // Data formatada
                 "^FT30,55^A0N,23,23^FH\\^CI28^FD" + horaFormatada + "^FS^CI27\n" +  // Hora formatada
-                "^FT30,300^A0N,18,18^FH\\^CI28^FDContador: " + contador + "^FS^CI27\n" +  // Contador
                 "^PQ2,0,1,Y\n" +
                 "^XZ";
 
@@ -138,16 +146,8 @@ public class TelnetClient {
        imprimirViaUSB(zpl);
        
        // Incrementar o contador e salvar de volta no arquivo
-       contador++;
-       try {
-		salvarContador(contador);
-	} catch (FileNotFoundException e) {
-		System.out.println("Erro file not found exception");
-		e.printStackTrace();
-	} catch (java.io.IOException e) {
-		System.out.println("Erro javaio exceexception");
-		e.printStackTrace();
-	}
+     
+	
     }
     
     public void imprimirViaUSB(String zplComando) {
@@ -185,44 +185,41 @@ public class TelnetClient {
             e.printStackTrace();
         }
     }
-    public <LocalDataTime> String gerarEtiquetaZPL(String placa, String destino, String valor, LocalDateTime  dataPesagem, BigDecimal peso) {
-      //  String pesoFormatado = String.format("%.3f", peso);
-
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String dataFormatada =  dataPesagem.format(dateFormatter);  // Usa 'format()' para a data
-
-        // Formata a hora como "HH:mm"
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-        String horaFormatada = dataPesagem.format(timeFormatter);  // Usa 'format()' para a hora
-
-
-    	    // Código ZPL gerado dinamicamente com os valores passados
-    	    String zpl = "^XA\n" +
-    	                 "^PW480\n" +
-    	                 "^LL320\n" +
-    	                 "^FT30,212^A0N,23,25^FH\\^CI28^FDPlaca:^FS^CI27\n" +
-    	                 "^FT115,215^A0N,23,25^FH\\^CI28^FD" + placa + "^FS^CI27\n" +  // Placa do veículo
-    	                 "^FT30,182^A0N,23,25^FH\\^CI28^FDDestino:^FS^CI27\n" +
-    	                 "^FT115,182^A0N,23,25^FH\\^CI28^FD" + destino + "^FS^CI27\n" +  // Destino
-    	                 "^FT30,255^A0N,23,25^FH\\^CI28^FDValor:^FS^CI27\n" +
-    	                 "^FT115,254^A0N,23,25^FH\\^CI28^FD" + valor + "^FS^CI27\n" +  // Valor
-    	                 "^FT125,114^A0N,39,51^FH\\^CI28^FD" + peso + "kg^FS^CI27\n" +  // Peso
-    	                 "^FT30,33^A0N,23,23^FH\\^CI28^FD" + dataFormatada + "^FS^CI27\n" +  // Data formatada
-    	                 "^FT30,55^A0N,23,23^FH\\^CI28^FD" + horaFormatada + "^FS^CI27\n" +  // Hora formatada
-    	                 "^PQ1,0,1,Y\n" +
-    	                 "^XZ";
-
-    	    return zpl;
-    	}
-
-    public  void salvarEmArquivo(String nomeArquivo, String conteudo) throws java.io.IOException {
-        try (FileWriter writer = new FileWriter(nomeArquivo)) {
-            writer.write(conteudo);
-            System.out.println("Arquivo ZPL salvo com sucesso: " + nomeArquivo);
-        } catch (IOException e) {
-            System.err.println("Erro ao salvar arquivo: " + e.getMessage());
-        }
-    }
+	/*
+	 * public <LocalDataTime> String gerarEtiquetaZPL(String placa, String destino,
+	 * String valor, LocalDateTime dataPesagem, BigDecimal peso) { // String
+	 * pesoFormatado = String.format("%.3f", peso);
+	 * 
+	 * DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	 * String dataFormatada = dataPesagem.format(dateFormatter); // Usa 'format()'
+	 * para a data
+	 * 
+	 * // Formata a hora como "HH:mm" DateTimeFormatter timeFormatter =
+	 * DateTimeFormatter.ofPattern("HH:mm"); String horaFormatada =
+	 * dataPesagem.format(timeFormatter); // Usa 'format()' para a hora
+	 * 
+	 * 
+	 * // Código ZPL gerado dinamicamente com os valores passados String zpl =
+	 * "^XA\n" + "^PW480\n" + "^LL320\n" +
+	 * "^FT30,212^A0N,23,25^FH\\^CI28^FDPlaca:^FS^CI27\n" +
+	 * "^FT115,215^A0N,23,25^FH\\^CI28^FD" + placa + "^FS^CI27\n" + // Placa do
+	 * veículo "^FT30,182^A0N,23,25^FH\\^CI28^FDDestino:^FS^CI27\n" +
+	 * "^FT115,182^A0N,23,25^FH\\^CI28^FD" + destino + "^FS^CI27\n" + // Destino
+	 * "^FT30,255^A0N,23,25^FH\\^CI28^FDValor:^FS^CI27\n" +
+	 * "^FT115,254^A0N,23,25^FH\\^CI28^FD" + valor + "^FS^CI27\n" + // Valor
+	 * "^FT125,114^A0N,39,51^FH\\^CI28^FD" + peso + "kg^FS^CI27\n" + // Peso
+	 * "^FT30,33^A0N,23,23^FH\\^CI28^FD" + dataFormatada + "^FS^CI27\n" + // Data
+	 * formatada "^FT30,55^A0N,23,23^FH\\^CI28^FD" + horaFormatada + "^FS^CI27\n" +
+	 * // Hora formatada "^PQ1,0,1,Y\n" + "^XZ";
+	 * 
+	 * return zpl; }
+	 * 
+	 * public void salvarEmArquivo(String nomeArquivo, String conteudo) throws
+	 * java.io.IOException { try (FileWriter writer = new FileWriter(nomeArquivo)) {
+	 * writer.write(conteudo); System.out.println("Arquivo ZPL salvo com sucesso: "
+	 * + nomeArquivo); } catch (IOException e) {
+	 * System.err.println("Erro ao salvar arquivo: " + e.getMessage()); } }
+	 */
 }
 	
     
